29b1f8b95ce2c6441c937a73b103633b4e97d1d6
danielallen88 <luv4daniel@gmail.com>
2015-03-18 13:14:50 +0800

    Adding logic

--- js/2048-core.js
+++ js/2048-core.js
@@ -35,17 +35,51 @@ var _GameViewModel = function () {
         });
     };
 
+    self.randNum = function (val){
+        var newVal = Math.floor((Math.random()) * (4 - 1)) + 1;
+
+        if (newVal === val) {
+            return self.randNum(val);
+        }
+
+        return newVal;
+    };
+
+    self.addTwo = function () {
+        var row = self.randNum(0);
+        var col = self.randNum(0);
+
+        ko.utils.arrayForEach(self.tiles(), function(tile) {
+            if (tile.row == row && tile.col == col && tile.val == 0) {
+                self.tiles.remove(tile);
+                self.tiles.push({row: row, col: col, val: 2});
+                console.log(tile);
+            }
+        });
+
+        var _row = self.randNum(row);
+        var _col = self.randNum(col);
+
+        ko.utils.arrayForEach(self.tiles(), function(tile) {
+            if (tile.row == _row && tile.col == _col && tile.val == 0) {
+                self.tiles.remove(tile);
+                self.tiles.push({row: _row, col: _col, val: 2});
+                console.log(tile);
+            }
+        });
+    };
+
     self.showThis = function(data, event) {
         console.log(data);
     }
-}
+};
 
 var GameViewModel = new _GameViewModel();
 
 //jQuery custom function for sorting knockout observableArray by Title/Date
 $.fn.sortTiles = function(sortBy) {
-    ToDoViewModel.tiles.sort(function(a, b) {
-        var _a = a.row, _b = b.row;
+    GameViewModel.tiles.sort(function(a, b) {
+        var _a = a.col, _b = b.col;
         if(_a == _b) return 0;
         return _a > _b ? 1 : -1;
     });
@@ -55,4 +89,23 @@ $.fn.sortTiles = function(sortBy) {
 $(function(){
     // Activates knockout.js
     ko.applyBindings(GameViewModel);
+    GameViewModel.addTwo();
+    $(document).sortTiles();
+
+    $(document).on( "keydown", function( event ) {
+        switch( event.which ) {
+            case 87:
+                console.log( "up" );
+                break;
+            case 83:
+                console.log( "down" );
+                break;
+            case 65:
+                console.log( "left" );
+                break;
+            case 68:
+                console.log( "right" );
+                break;
+        }
+    });
 });
